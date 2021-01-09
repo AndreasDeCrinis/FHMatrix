67f8b4c7a161754d6f45f8e1b0cf4bc9ee28a7c2
and.kappel <and.kappel@gmail.com>
2021-01-09 08:44:50 +0100

    initial commit

---
+++ compile.ps1
@@ -0,0 +1,68 @@
+$matrix = (get-content $PSScriptRoot\matrix2.csv) | ConvertFrom-Csv -Delimiter ';'
+$header = ((Get-Content -Path $PSScriptRoot\matrix2.csv | select -First 1) -split ";").Trim("`"")
+
+$output = @()
+$id = 0
+
+foreach ($m in $matrix) {
+    $header = $m.PSObject.Properties.Name
+
+
+    if ($m.LV.ToLower() -match "semester" -or $m.LV -eq "") {
+        continue
+    }
+    $indexFirstSpace = $m.LV.IndexOf(' ')[0] + 1
+    $length = $m.LV.Length
+    $lvs = @{
+        LV           = $m.LV.Substring($indexFirstSpace, $length - $indexFirstSpace)
+        id           = $id
+        dependencies = @()
+        group=$m.LV.Substring(0,$indexFirstSpace-1)
+    }
+    foreach ($h in $header) {
+        if ($h -eq "LV") { continue }
+
+        $slimIndexFirstSpace = $h.IndexOf(' ')[0] + 1
+        $slimHeader = $h.Substring($slimIndexFirstSpace, $h.Length - $slimIndexFirstSpace)
+
+        if ($m.$h -eq "x") {
+            $lvs.dependencies += $slimHeader
+        }
+    }
+    $output += $lvs
+    $id++
+}
+
+$nodes = @()
+$edges = @()
+foreach ($out in $output) {
+    # { id: 0, label: "Berufspraktikum", group: 1 },
+    $group = $out.group.Split('.')[0]
+    # $nodes += "{ id: $($out.id), label: `"$($out.LV)`", group: $group},"
+    $nodes += @{
+        id    = $out.LV
+        group = $group
+    }
+
+    foreach ($dependency in $out.dependencies) {
+        $dep = $output | where { $_.LV -eq $dependency }
+        $edges += @{
+            source = $out.LV
+            target = $dep.LV
+            value  = $group -as [int]
+        }
+    }
+}
+
+# ((Get-Content -path $PSScriptRoot\index_template.html -Raw) -replace '<nodes>',$nodes) | Set-Content -Path $PSScriptRoot\index.html
+# ((Get-Content -path $PSScriptRoot\index.html -Raw) -replace '<edges>',$edges) | Set-Content -Path $PSScriptRoot\index.html
+
+$outputObject = @{
+    nodes = $nodes
+    links = $edges
+}
+# $outputObject | ConvertTo-Json | Out-File $PSScriptRoot\data.json
+
+$tag = "v10"
+docker build --tag container.kandreas.work/matrix:$tag $PSScriptRoot
+docker push container.kandreas.work/matrix:$tag
